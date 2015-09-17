58bae41af86e99d4af0c9e389e7d5557c5830ce6
Chris Mungall <cjm@berkeleybop.org>
2015-09-17 15:54:59 -0700

    new. #8

--- src/medgen/medgen2obo.pl
+++ src/medgen/medgen2obo.pl
@@ -3,13 +3,20 @@ use strict;
 
 my %th = ();
 my %rh = ();
+my %dh = ();
+my %uh = ();
+my %ssh = ();
+my %styh = ();
 
 our $PATH = "ftp.ncbi.nlm.nih.gov/pub/medgen";
 
 open(F,"gzip -dc $PATH/MGCONSO.RRF.gz|") || die;
 while(<F>) {
+    next if m@^#@;
     chomp;
     my ($CUI, $TS, $STT, $ISPREF, $AUI, $SAUI, $SCUI, $SDUI, $SAB, $TTY, $CODE, $STR, $SUPPRESS) = split(/\|/,$_);
+    $STR =~ s/\{/\\\{/g;
+    $STR =~ s/\}/\\\}/g;
     if (!$th{$CUI}->{name} || $TS eq 'P') {
         $th{$CUI}->{name} = $STR;
     }
@@ -33,6 +40,7 @@ close(F);
 
 open(F,"gzip -dc $PATH/MGREL.RRF.gz|") || die;
 while(<F>) {
+    next if m@^#@;
     chomp;
     my (
         $CUI1  , # first concept unique identifier
@@ -54,7 +62,44 @@ while(<F>) {
 }
 close(F);
 
-print "ontology: medgen\n\n";
+
+
+open(F,"gzip -dc $PATH/MGSTY.RRF.gz|") || die;
+while(<F>) {
+    next if m@^#@;
+    chomp;
+    my ($CUI, $TUI, $STN, $STY, $ATUI) = split(/\|/, $_);
+    $ssh{$CUI}->{$STY} = 1;
+    $styh{$STY}++;
+}
+close(F);
+
+
+open(F,"uid2cui.tsv") || die;
+while(<F>) {
+    next if m@^#@;
+    chomp;
+    my ($u,$c) = split(/\t/,$_);
+    $uh{$c} = $u;
+    $th{$c}->{xrefs}->{"MEDGEN:$u"} = 1;
+}
+close(F);
+
+#open(F,"gzip -dc $PATH/MGDEF.RRF.gz|") || die;
+#while(<F>) {
+#    chomp;
+#    my ($CUI, $DEF, $SOURCE) = split(/\|/, $_);
+#    $dh{$CUI}->{$SOURCE} = $DEF;
+#}
+#close(F);
+
+print "ontology: medgen\n";
+foreach (keys %styh) {
+    my $ss = mk_subset($_);
+    print "subsetdef: $ss \"$_\"\n";
+}
+print "\n";
+
 my @ids = keys %th;
 @ids = sort @ids;
 foreach my $id (@ids) {
@@ -65,8 +110,13 @@ foreach my $id (@ids) {
     foreach my $x (keys %{$h->{xrefs}}) {
         print "xref: $x\n";
     }
+    foreach (keys %{$ssh{$id} || {}}) {
+        my $ss = mk_subset($_);
+        print "subset: $ss\n";
+    }
     foreach my $s (@{$h->{synonyms}}) {
         my ($str, $x)= @$s;
+        $str = escq($str);
         print "synonym: \"$str\" RELATED [$x]\n";
     }
     my $trelh = $rh{$id};
@@ -86,4 +136,20 @@ foreach my $id (@ids) {
         }
     }
     print "\n";
+}
+
+exit 0;
+
+sub mk_subset {
+    my $sty = shift;
+    $sty =~ s/\s+/-/g;
+    return $sty;
+}
+
+sub escq {
+    my $s = shift;
+    $s =~ s/\"/\'/g;
+    $s =~ s/\r/ /g;
+    $s =~ s/\n/ /g;
+    return $s;
 }
