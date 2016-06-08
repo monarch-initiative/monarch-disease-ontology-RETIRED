cccde277cee430c4b3cffb411e4115442b516e1a
Chris Mungall <cjm@berkeleybop.org>
2016-06-08 14:24:24 -0700

    Fixing obsoletes. Fixes #96

--- src/orphanet/fix-ordo.pl
+++ src/orphanet/fix-ordo.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
 
+my @out = ();
 while(<>) {
-    s@&obo;http://www.obofoundry.org/ro/ro.owl#part_of@&obo;BFO_0000050@g;
+    # no longer required
+    #s@&obo;http://www.obofoundry.org/ro/ro.owl#part_of@&obo;BFO_0000050@g;
     s@MeSH@MESH@;s@ICD-10@ICD10@;
-    print;
-
+    if (m@^is_a: http://www.orpha.net/ORDO/ObsoleteClass@) {
+        s@^is_a: http://www.orpha.net/ORDO/ObsoleteClass.*@is_obsolete: true@;
+        $out[-1] =~ s@name: @name: obsolete @;
+    }
+    s@^property_value: http://www.ebi.ac.uk/efo/reason_for_obsolescence "use http://www.orpha.net/ORDO/(\S+)_(\S+).*@replaced_by: $1:$2@;
+    push(@out, $_);
 }
+print foreach @out;
