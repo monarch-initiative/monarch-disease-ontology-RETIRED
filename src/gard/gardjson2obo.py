56e26f9f84ec752070e29fee8676bf25a6656de3
cmungall <cjm@berkeleybop.org>
2021-02-04 19:18:35 -0800

    removing cruft and repairing

--- src/gard/gardjson2obo.py
+++ src/gard/gardjson2obo.py
@@ -8,7 +8,9 @@ obj = json.load(open('gard.json'))
 for d in obj:
     print(f'[Term]')
     print(f"id: GARD:{d['diseaseId']}")
-    print(f"name: {d['diseaseName']}")
+    n = d['diseaseName'].strip()
+    n = n.replace("\t","")
+    print(f"name: {n}")
     if 'websiteUrl' in d:
         print(f"property_value: http://www.w3.org/2000/01/rdf-schema#seeAlso {d['websiteUrl']}")
     if 'isRare' in d:
